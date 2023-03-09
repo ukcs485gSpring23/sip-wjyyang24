5e6f40e46e8bba8eb28289aa0887169f51be289a
Wesley Yang <wjyyang24@gmail.com>
2023-03-09 02:22:53 -0500

    Finished changes to stylers

---
+++ OCKSample/Main/Stylers/DimensionStyle.swift
@@ -0,0 +1,18 @@
+//
+//  DimensionStyle.swift
+//  OCKSample
+//
+//  Created by Wesley Yang on 3/9/23.
+//  Copyright © 2023 Network Reconnaissance Lab. All rights reserved.
+//
+
+import Foundation
+import CareKitUI
+
+struct DimensionStyle: OCKDimensionStyler {
+    var lineWidth1: CGFloat { 8 }
+    var stackSpacing1: CGFloat { 16 }
+    var pointSize3: CGFloat { 22 }
+    var pointSize2: CGFloat { 28 }
+    var pointSize1: CGFloat { 34 }
+}
